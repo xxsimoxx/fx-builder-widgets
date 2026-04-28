611ad44dc1d07ac0fe75c264cb04ca1098e77e3c
xxsimoxx <simone@gieffeedizioni.it>
2026-04-28 08:29:36 +0200

    Refactor uninstall process

--- uninstall.php
+++ uninstall.php
@@ -3,16 +3,14 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
 
-if (!defined('FBW_REMOVE_CONTENT_ON_UNINSTALL') || FBW_REMOVE_CONTENT_ON_UNINSTALL !== true) {
-	exit;
-}
+if (defined('FBW_REMOVE_CONTENT_ON_UNINSTALL') && FBW_REMOVE_CONTENT_ON_UNINSTALL === true) {
+	$post_list = get_posts([
+		'post_type'      => 'fx-builder-content',
+		'posts_per_page' => -1,
+		'post_status'    => 'any',
+	]);
 
-$post_list = get_posts([
-	'post_type'      => 'fx-builder-content',
-	'posts_per_page' => -1,
-	'post_status'    => 'any',
-]);
-
-foreach ($post_list as $p) {
-	wp_delete_post($p->ID, true);
+	foreach ($post_list as $p) {
+		wp_delete_post($p->ID, true);
+	}
 }
